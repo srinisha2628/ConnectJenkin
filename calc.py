a2fea8872d4ebff83fe1c30cb8b467a44650802b
Jees <jeeskonnackal@gmail.com>
2020-03-04 11:59:38 +0530

    test calc commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+#import sys
+#a=int(sys.argv[1])
+#b=int(sys.argv[2])
+def addop(a,b):
+    return a+b
+def subop(a,b):
+    return a-b
+def mulop(a,b):
+        return a*b
+def divop(a,b):
+        return a/b
